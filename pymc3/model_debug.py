eb688cfa560d69a2a5c8f3f3cdc8456e915e831e
Ricardo <ricardo.vieira1994@gmail.com>
2021-02-09 18:51:26 +0100

    Mask culprit values

--- pymc3/model_debug.py
+++ pymc3/model_debug.py
@@ -11,6 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+from pprint import pformat
 
 import numpy as np
 import theano
@@ -26,21 +27,20 @@ def find_infinite_bounds(apply, implicit_bounds=False, bound_nodes=None):
     # Check if it is a switch node
     # print(apply.op.scalar_op)
     if hasattr(apply.op, "scalar_op") and isinstance(apply.op.scalar_op, theano.scalar.basic.Switch):
+        switch_input, *switch_outputs = apply.inputs  # 1 input, 2 outputs
+
         # Check if it is an explicit bound switch (named)
-        switch_input = apply.inputs[0]
         if apply.out.name == 'bound_switch':
-            bound_nodes.append((switch_input, 'bound'))
+            bound_nodes.append((switch_input, switch_outputs, 'explicit_bound'))
             # Terminate graph trasversal here
             if not implicit_bounds:
                 return bound_nodes
         # Check if it is an implicit bound switch (leads to -inf)
         elif implicit_bounds:
-            switch_outputs = apply.inputs[1:]
             if switch_outputs[0].eval() == -np.inf:
-                bound_nodes.append((switch_input, 'expression_true'))
-            # elif check_leaf_infinite_constant(switch_outputs[1]):
-            elif switch_outputs[1].eval() == -np.inf:
-                bound_nodes.append((switch_input, 'expression_false'))
+                bound_nodes.append((switch_input, switch_outputs, 'implicit_bound_true'))
+            if switch_outputs[1].eval() == -np.inf:
+                bound_nodes.append((switch_input, switch_outputs, 'explicit_bound_false'))
 
         # Continue to explore downstream of the switch
         child_apply = switch_input.owner
@@ -61,7 +61,6 @@ def find_infinite_bound_logical_conds(apply, logical_conds=None):
     if logical_conds is None:
         logical_conds = []
 
-    # TODO: include unary logic operators?
     if hasattr(apply.op, "scalar_op") and isinstance(apply.op.scalar_op, theano.scalar.basic.LogicalComparison):
         logical_conds.append(apply)
 
@@ -84,27 +83,33 @@ def find_logical_cond_input_variables(logical_cond_apply):
         if not expression.owner:
             input_variables.append((expression,))
         else:
-            # TODO: This may fail? Have a fallback
             expression_inputs = tuple(theano.graph.basic.graph_inputs(expression.owner.inputs))
 
-            # Check for 1.0 * var expression introduced by the bound_switch
+            # TODO: Check for 1.0 * var expression introduced by the bound_switch
+            # and remove Constant(1) from output
+            found_mul_1 = False
             for potential_var in expression_inputs:
                 test_expression = 1.0 * potential_var
                 if is_same_graph(expression, test_expression):
                     input_variables.append((potential_var,))
-                    break
-            else:  # nobreak
+                    found_mul_1 = True
+            # Otherwise, include all inputs
+            if not found_mul_1:
                 input_variables.append(expression_inputs)
 
     return input_variables
 
 
-def input_variables_to_string(x):
+# TODO: Less hackish way to mask culprit values
+def input_variables_to_string(x, mask):
+    def get_masked_values(x):
+        return np.squeeze(np.unique(np.resize(x, mask.shape)[mask]))
+
     def get_value_if_const(x):
         if hasattr(x, 'value'):
-            return x.value
+            return get_masked_values(x.value)
         if hasattr(x, 'tag'):
-            return f"{x}: [{x.tag.test_value}]"
+            return f"{x} = {get_masked_values(x.tag.test_value)}"
         return x
 
     if len(x) == 1:
@@ -122,25 +127,40 @@ theano_logical_comparators_parse_map = {
     theano.scalar.basic.LE: '<=',
 }
 
-
+# TODO: Flip logic for implicit bounds with expression_false
+# TODO: Print only non-repeated bad values (and trim output)
+# TODO: Get parameter / observed names (is this possible)?
+# TODO: Add logic for BinaryBitOps ?
 def debug_bounds(model, variable, implicit_bounds=False):
     bound_switches = find_infinite_bounds(variable.logpt.owner, implicit_bounds)
     # Test bound
-    for bound_switch, bound_description in bound_switches:
-        # Check if bound evaluates to False (TODO: Or True for implicit bounds with expression_false)
-        bound_fn = model.fn(bound_switch)
+    for bound_switch_input, bound_switch_outputs, bound_description in bound_switches:
+
+        # Check that bound input evaluates to False (otherwise it cannot be responsible)
+        bound_fn = model.fn(bound_switch_input)
         bound_output = bound_fn(model.test_point)
-        print(bound_switch, bound_output)
+        # print(bound_switch, bound_output)
         if np.all(bound_output):
+            # If it's an explicit bound_switch, confirm that the nested logp expression is non-finite
+            if bound_description == 'explicit_bound':
+                logp_branch = bound_switch_outputs[0]
+                logp_fn = model.fn(logp_branch)
+                logp = logp_fn(model.test_point)
+                if not np.all(np.isfinite(logp)):
+                    print(
+                        f'The logp expression of {variable} is non-finite for the given inputs,',
+                        'but no explicit bounds were found to have been violated.'
+                     )
             continue
 
-        bound_logical_conds = find_infinite_bound_logical_conds(bound_switch.owner)
+        # If bound is responsible, test which nested logical conditions are responsible
+        bound_logical_conds = find_infinite_bound_logical_conds(bound_switch_input.owner)
         first_bound = True
-        # Test bound logical conditions
 
         # 1. Sanity check that disabling all logical conditions leads the switch to be true
+        # TODO: Remove?
         no_bound = theano.clone(
-            bound_switch,
+            bound_switch_input,
             {
                 logical_cond.out: tt.eq(logical_cond.inputs[0], logical_cond.inputs[0])
                 for logical_cond in bound_logical_conds
@@ -154,10 +174,9 @@ def debug_bounds(model, variable, implicit_bounds=False):
 
         # 2. Enable one switch at a time (culprit if switch stays false)
         for enabled_logical_cond in bound_logical_conds:
-
             # TODO: Is there a more clean way to achieve this test?
             new_bound = theano.clone(
-                bound_switch,
+                bound_switch_input,
                 {
                     logical_cond.out: tt.eq(logical_cond.inputs[0], logical_cond.inputs[0])
                     for logical_cond in bound_logical_conds if logical_cond != enabled_logical_cond
@@ -165,20 +184,24 @@ def debug_bounds(model, variable, implicit_bounds=False):
             )
             new_bound_fn = model.fn(new_bound)
             new_bound_output = new_bound_fn(model.test_point)
-            print('Enabled: ', enabled_logical_cond, ' | Result :', new_bound_output)
+            # print('Enabled: ', enabled_logical_cond, ' | Result :', new_bound_output)
             if not np.all(new_bound_output):
                 if first_bound:
                     first_bound = False
-                    explicit = 'explicit' if bound_description == 'bound' else 'implicit'
+                    explicit = 'explicit' if bound_description == 'explicit_bound' else 'implicit'
                     print(f'The following {explicit} bound(s) of {variable} were violated:')
-                    print('')
 
                 # print(enabled_logical_cond.out)
+                mask = new_bound_output == 0
+                # print(new_bound_output, mask)
                 ivs = find_logical_cond_input_variables(enabled_logical_cond)
-                ivs = [input_variables_to_string(iv) for iv in ivs]
-                logical_comp = theano_logical_comparators_parse_map[(type(enabled_logical_cond.op.scalar_op))]
+                ivs = [input_variables_to_string(iv, mask) for iv in ivs]
+                logical_cond_type = type(enabled_logical_cond.op.scalar_op)
+                logical_comp = theano_logical_comparators_parse_map.get(
+                    logical_cond_type,
+                    str(logical_cond_type),
+                )
                 print(f'{ivs[0]} {logical_comp} {ivs[1]}')
-                print('')
 
 
 def debug_bad_energy(model, implicit_bounds=False):
@@ -187,3 +210,4 @@ def debug_bad_energy(model, implicit_bounds=False):
         if not(np.isfinite(variable.logp(test_point))):
             debug_bounds(model, variable, implicit_bounds)
     print('Done')
+
